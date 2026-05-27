51a138424af24dfb1a40017ae8064822bd1199f4
mariocarbache <mariocarbache@gmail.com>
2026-05-27 11:23:32 -0400

    added basic project structure/routes

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,8 @@
+export default function OnboardingPage() {
+    return(
+        <main>
+            <h1>Pick your teams</h1>
+            <p>Coming in phase 3</p>
+        </main>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Tifo</h1>
-      <p>Coming Soon</p>
+      <nav>
+        <ul>
+          <li><Link href="/dashboard">Dashboard</Link></li>
+          <li><Link href="/login">Login</Link></li>
+          <li><Link href="/onboarding">Onboarding</Link></li>
+        </ul>
+      </nav>
     </main>
   );
 }
